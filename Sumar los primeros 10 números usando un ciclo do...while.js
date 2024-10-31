089b18240cd0fc3d9c905f47506100ca31f8e3b4
AndersonSaulMaravillaCallejas <164236833+5iranderson12@users.noreply.github.com>
2024-10-31 11:42:45 -0600

    Create Sumar los primeros 10 números usando un ciclo do...while.js

---
+++ Sumar los primeros 10 números usando un ciclo do...while.js
@@ -0,0 +1,8 @@
+// Sumar los primeros 10 números usando un ciclo do...while
+let j = 1;
+let sum = 0;
+do {
+  sum += j;
+  j++;
+} while (j <= 10);
+console.log("Suma de los primeros 10 números:", sum);
